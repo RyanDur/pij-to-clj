eed3def9a35c4a8ca717c8416bec503479c15544
RyanDur <ryan.p.durling@gmail.com>
2015-06-14 20:48:30 -0500

    q 5

---
+++ src/pij_to_clj/multiplication.clj
@@ -0,0 +1,13 @@
+(ns pij-to-clj.multiplication
+  (:gen-class))
+
+;; Write a program that requests two numbers from
+;; the user and then outputs its product. You
+;; cannot use the “*” operator.
+
+(println "please enter a number")
+(def a (read-string (read-line)))
+(println "please enter another number")
+(def b (read-string (read-line)))
+
+(println (reduce + (repeat b a)))
