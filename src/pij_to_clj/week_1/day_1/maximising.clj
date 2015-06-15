cedc2905c10f854f11d515a41ee82b2dd27a83ff
RyanDur <ryan.p.durling@gmail.com>
2015-06-15 12:25:19 -0500

    q 8

---
+++ src/pij_to_clj/week_1/day_1/maximising.clj
@@ -0,0 +1,16 @@
+(ns pij-to-clj.week-1.day-1.maximising
+  (:gen-class))
+
+;; Write a program that read a (arbitrarily long) sequence
+;; of positive numbers. The sequence is ended when the
+;; users enters “-1”. At that point, the program must output
+;; the highest number in the sequence.
+
+(defn get-input
+  []
+  (do (print "Input a number, enter -1 to stop ")
+      (flush)
+      (read-string (read-line))))
+
+
+(apply max (take-while #(not (= % -1)) (repeatedly get-input)))
